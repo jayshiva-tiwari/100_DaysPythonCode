2e22b5c4c9343587e0c59cc13e0d9fea95906845
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2025-06-11 00:56:05 +0530

    Day 01 in python

---
+++ day_01.py
@@ -0,0 +1,6 @@
+# variables and data types
+name = "Shiva"
+age = 21
+
+# f-strings or formatted strings is a way to insert variables into a string
+print(f"Hi, I'm {name} and I'm {age} years old.")
