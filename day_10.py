5c82efe378266cf381fd770940b73d45b51582e1
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2025-06-20 09:28:29 +0530

    Day 10 Modules and packages

---
+++ day_10.py
@@ -0,0 +1,19 @@
+# Day 10 with programming paglu 🎀
+
+# Build a custom module (like a math toolkit or greeter)?
+from packages import greet
+
+print(greet.say_hello("shiva"))
+
+# Learn to use built-in Python modules like math, random, datetime?
+
+from packages import math
+
+output = math.math(4, 2)
+print(f"your output: {output}")
+
+# greet_user(name) → "Namaste, Shiva ji!"
+def greet(name):
+    print(f"Namaste 🙏, {name}")
+
+greet("shiva ji🔥")
