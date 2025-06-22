0f80d56a9583f08b4b31707888bb88716707773e
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2025-06-22 21:27:40 +0530

    Day 12 API

---
+++ day-12.py
@@ -0,0 +1,13 @@
+# Day 12 with programming🎀paglu !!
+
+# What is an API?
+# An API (Application Programming Interface) lets your Python program talk to other services or websites to get or send data.
+
+import requests
+
+response = requests.get("https://official-joke-api.appspot.com/random_joke")
+joke = response.json()
+
+print("Here's a joke for you 🤣:")
+print(joke["setup"])
+print(joke["punchline"])
