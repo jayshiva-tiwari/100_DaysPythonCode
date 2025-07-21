df8b403660d2c1ca978396ed6566726baec8566c
jayshiva-tiwari <139352267+jayshiva-tiwari@users.noreply.github.com>
2025-07-21 11:12:14 +0530

    Day 41 with Review

---
+++ day_41.py
@@ -0,0 +1,17 @@
+# Day 41 of Python with Programming Paglu 🎀
+
+# Review to concepts of python
+
+# What I Learned:
+    # 📦 requests, json, csv, os, shutil, smtplib, bs4
+
+    # 🛠️ Real-world automation tools
+
+    # 📤 Sending emails + scraping data
+
+    # 📁 API-based tools and file logging
+
+    # 🧩 Creating fun CLI tools from public data
+
+
+
